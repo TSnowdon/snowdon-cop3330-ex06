93b795c437257e3123e3c87ee0744fbc2abf3138
CarrotChomps <snowdonkid@gmail.com>
2021-09-02 15:12:49 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 6 Solution
+ *  Copyright 2021 Tyler Snowdon
+ */
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstInput;
+        String secondInput;
+        int age;
+        int retireAge;
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        Scanner reader = new Scanner(System.in);
+        System.out.print("What is your current age? ");
+        firstInput = reader.nextLine();
+        System.out.print("At what age would you like to retire? ");
+        secondInput = reader.nextLine();
+
+        try {
+            age = Integer.parseInt(firstInput);
+            retireAge = Integer.parseInt(secondInput);
+        } catch (Exception e) {
+            System.out.println("Invalid input! Please run the program again and try a different value.");
+            return;
+        }
+
+        int yearLeft = retireAge - age;
+
+        System.out.println("You have " + yearLeft + " years left until you retire");
+        System.out.println("It's " + currentYear + " so you can retire in " + (currentYear + yearLeft));
+    }
+}
